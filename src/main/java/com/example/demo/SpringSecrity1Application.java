a870013f6b6b4f8b5c6aaee53fed6493490d975e
hiroyuki_yamashita <hirokore3221@gmail.com>
2022-05-10 22:52:37 +0900

    first commit

---
+++ src/main/java/com/example/demo/SpringSecrity1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecrity1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecrity1Application.class, args);
+	}
+
+}
